bca27cd03f59c7e9a180a82e2b49b1c8d93d9190
varubiom1 <rubiom.vane@gmail.com>
2020-04-02 22:28:24 -0500

    vrubio

--- DevWebPro/src/app/app.component.ts
+++ DevWebPro/src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DevWebPro';
+  title = '¡HOLI! BIENVENIDO A NUESTRA APP';
+  name = 'JaiVaneTiven';
+  num1 =2;
+  num2 =3;
+  resultSuma: number;
+
+
+  /**
+   * Constructor function of the app component
+   */
+
+  constructor(){
+    this.resultSuma=0;
+    console.log('CONSTRUCTOR...APP component created!!!');
+    console.log('RESULT SUMA:', this.resultSuma);
+  }
+/**
+ * function to add two numers
+ */
+  sumar (){
+    console.log('SUMAR');
+    this.resultSuma= this.num1 + this.num2;
+  }
 }
--- DevWebPro/src/environments/environment.prod.ts
+++ DevWebPro/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  APP_NAME: "APP POLI",
 };
