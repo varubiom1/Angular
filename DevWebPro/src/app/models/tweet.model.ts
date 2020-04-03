208d1fcc4ddc3524d700277b2a0405ee230b1cfe
varubiom1 <rubiom.vane@gmail.com>
2020-04-02 23:19:30 -0500

    Vrubio

---
+++ DevWebPro/src/app/models/tweet.model.ts
@@ -0,0 +1,5 @@
+export class Tweet{
+    id: number;
+    texto: string;
+    autor: string;
+}
--- DevWebPro/src/app/tweet/tweet.component.ts
+++ DevWebPro/src/app/tweet/tweet.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Tweet} from '../models/tweet.model';
 @Component({
   selector: 'app-tweet',
   templateUrl: './tweet.component.html',
   styleUrls: ['./tweet.component.css']
 })
 export class TweetComponent implements OnInit {
+ miTweet: Tweet;
 
-  constructor() { }
+
+  constructor() {
+    this.miTweet={
+      id: 1,
+      texto: "Creando nuestro primer tweet..emotional!",
+      autor: "JaiVaneTiven"
+    };
+   }
 
   ngOnInit(): void {
   }
