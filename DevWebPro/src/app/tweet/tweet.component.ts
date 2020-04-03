1ba5c63708843dcc29cafbde2ddb34c4d28a3bf4
varubiom1 <rubiom.vane@gmail.com>
2020-04-03 18:00:34 -0500

    jsuarez

--- DevWebPro/src/app/tweet/tweet.component.ts
+++ DevWebPro/src/app/tweet/tweet.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgModel} from '@angular/forms';
 import {Tweet} from '../models/tweet.model';
 @Component({
   selector: 'app-tweet',
@@ -8,36 +9,39 @@ import {Tweet} from '../models/tweet.model';
 export class TweetComponent implements OnInit {
  
   listaTweets = [];
-   nuevoTweet:Tweet;
+  nuevoTweet:Tweet;
 
 
   constructor() {
     this.nuevoTweet={
-      id: -1,
-      texto: "",
-      autor: ""
+      id: null,
+      texto: undefined,
+      autor: undefined
     };
     let miTweet={
       id: 1,
       texto: "Creando nuestro primer tweet..emotional!",
       autor: "JaiVaneTiven"
     };
-    let miTweet2={
-      id: 2,
-      texto: "RICO PA RICO",
-      autor: "Jai"
-    };
     this.listaTweets.push(miTweet);
-    this.listaTweets.push(miTweet2);
-    console.log("LISTA:",this.listaTweets); 
    }
-
-   adicionarNuevoTweet(){
-     
-     this.listaTweets.push(this.nuevoTweet);
-   }
-
-  ngOnInit(): void {
+  
+   ngOnInit(): void {
   }
 
-}
+
+
+   adicionarNuevoTweet(){
+    if (this.nuevoTweet.texto && this.nuevoTweet.autor) {
+      this.listaTweets.push({ ...this.nuevoTweet, id: this.listaTweets.length });
+      this.nuevoTweet = {
+        id: null,
+        texto: undefined,
+        autor: undefined
+      }
+    }
+  }
+ }
+
+ 
+
